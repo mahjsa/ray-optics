045ced2305b42b443dd2496bbbc7f470bbd7f390
mahjsa <57534176+mahjsa@users.noreply.github.com>
2021-01-19 12:04:02 +0100

    Add files via upload

---
+++ locales/nl.js
@@ -0,0 +1,141 @@
+﻿if (typeof locales == 'undefined') locales = {};
+locales["nl"] = {
+  "lang": {
+    "message": "Nederlands"
+  },
+  "font": {
+    "message": "Arial"
+  },
+  "page_title": {
+    "message": "Lichtstralen Simulatie - Home"
+  },
+  "title": {
+    "message": "Lichtstralen Simulatie"
+  },
+  "description": {
+    "message": "Een open-source web applicatie om reflectie en breking van licht te simuleren.<br>Written in HTML, CSS and JavaScript."
+  },
+  "start": {
+    "message": "Start Applicatie"
+  },
+  "github_link": {
+    "message": "Bekijk op GitHub"
+  },
+  "start-old": {
+    "message": "Oude Versie"
+  },
+  "tools": {
+    "message": "Tools"
+  },
+  "ray": {
+    "message": "Straal"
+  },
+  "ray_desc": {
+    "message": "Een lichtstraal door twee punten te kiezen."
+  },
+  "beam": {
+    "message": "Bundel"
+  },
+  "beam_desc": {
+    "message": "A parallele lichtbundel uit een lijnstuk, dichtheid wordt gekozen met de \"Straaldichtheid\" schuif."
+  },
+  "point_source": {
+    "message": "Punt lichtbron"
+  },
+  "point_source_desc": {
+    "message": "Lichtstralen komen uit een punt, aantal stralen wordt gekozen met de \"Straaldichtheid\" schuif."
+  },
+  "mirror": {
+    "message": "Spiegel"
+  },
+  "mirror_desc": {
+    "message": "Simuleer de reflectie van licht in een spiegel."
+  },
+  "mirror_arc": {
+    "message": "Spiegel (Boog)"
+  },
+  "mirror_arc_desc": {
+    "message": "Een spiegel als boog van een cirkel, die door drie punten is gedefinieerd."
+  },
+  "ideal_curved_mirror": {
+    "message": "Ideale gebogen spiegel"
+  },
+  "ideal_curved_mirror_desc": {
+    "message": "De ideale \"gebogen\" spiegel die exact de relatie (1/p + 1/q = 1/f) volgt. De brandpunt afstand (in pixels) kan worden ingesteld."
+  },
+  "glass_halfplane": {
+    "message": "Glas"
+  },
+  "glass_halfplane_desc": {
+    "message": "Simuleer de breking en spiegeling op een oppervlak. De intensiteiten worden berekend op basis van niet-gepolariseerd licht."
+  },
+  "glass_circle": {
+    "message": "Glas (Cirkel)"
+  },
+  "glass_circle_desc": {
+    "message": "Cirkelvormig glas, vastgelegd door het middelpunt en een punt aan het oppervlak."
+  },
+  "glass": {
+    "message": "Glas (andere vormen)"
+  },
+  "glass_desc": {
+    "message": "Glas met elke vorm die gemaakt kan worden met lijnstukken en cirkelbogen, inclusief prisma\'s en \"ronde\" lenzen."
+  },
+  "ideal_lens": {
+    "message": "Glas (Ideale lens)"
+  },
+  "ideal_lens_desc": {
+    "message": "Een ideale lens die de lenzenformule (1/p + 1/q = 1/f) volgt. De brandpuntafstand (in pixels) kan worden ingesteld."
+  },
+  "blocker": {
+    "message": "Blok"
+  },
+  "blocker_desc": {
+    "message": "Een lijnstuk dat geen lichtstralen doorlaat (dus absorbeert)."
+  },
+  "ruler": {
+    "message": "Lineaal"
+  },
+  "ruler_desc": {
+    "message": "Een lineaal die meet van een nulpunt tot een ander punt. De schaal is in pixels."
+  },
+  "protractor": {
+    "message": "Hoekmeter"
+  },
+  "protractor_desc": {
+    "message": "Een hoekmeter die een hoek meet vanaf een lijn door het centrum en een punt. De schaal is in graden."
+  },
+  "views": {
+    "message": "Weergaven"
+  },
+  "rays": {
+    "message": "Stralen"
+  },
+  "rays_desc": {
+    "message": "Toont de lichtstralen. Als de \"Straaldichtheid\" groot is, dan wordt het een continue bundel."
+  },
+  "extended_rays": {
+    "message": "Uitgebreide stralen"
+  },
+  "extended_rays_desc": {
+    "message": "Toont zowel de stralen als de uitgebreide stralen (door reflectie). Oranje betekent virtuele stralen, grijs zijn de gereflecteerde stralen."
+  },
+  "all_images": {
+    "message": "Alle beelden"
+  },
+  "all_images_desc": {
+    "message": "Toont de positie van alle beelden. Gele punten zijn reeele beelden, oranje zijn virtuele beelden en grijs (niet zichtbaar in dit voorbeeld) zijn virtuele objecten. Let op dat sommige beelden niet getoond kunnen worden als de \"Straaldichtheid\" niet groot genoeg is."
+  },
+  "seen_by_observer": {
+    "message": "Zichtbaar door observator"
+  },
+  "seen_by_observer_desc": {
+    "message": "Simuleert de stralen en beelden zoals die vanuit een bepaalde positie te zien zijn. De blauwe cirkel stelt de waarnemer (een oog) voor. Elke straal die hier doorheen gaat wordt beschouwd al \"zichtbaar\". De waarnemer weet niet waar de stralen in werkelijkheid beginnen, maar gaat uit van rechte lijnen vanaf het punt van waarneming. De stralen worden weergegeven in blauw, de (virtuele) lichtbronnen in oranje."
+  },
+  "language": {
+    "message": "Language: Nederlands"
+  },
+  "footer_message": {
+    "message": "Developed by <a href='https://github.com/ricktu288' style='color:rgba(255,255,255,0.25)'>ricktu288</a>, <a href='https://github.com/j3soon' style='color:rgba(255,255,255,0.25)'>j3soon</a>"
+  }
+};
